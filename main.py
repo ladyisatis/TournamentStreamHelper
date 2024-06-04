5906e140feccb35f37b23de142d6e80bb0d19dfe
ladyisatis <isatis@foxes.gay>
2024-06-03 17:14:48 -0700

    use hypercorn asyncio instead of uvicorn

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import sys
 import multiprocessing
-import uvicorn
+import asyncio
+
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 from socketio import ASGIApp
-from server import app, config
 from loguru import logger
+
+from server import app, settings
+
+async def main():
+    logger.info("Server is starting")
+
+    try:
+        await settings.load()
+        combined_app = ASGIApp(
+            app.socketio,
+            app
+        )
+        
+        config = Config()
+        host = settings.get("server.host")
+        port = settings.get("server.port")
+        config.bind = [f"{host}:{port}"]
+
+        await serve(combined_app, config)
+        return 0
+    except:
+        logger.exception("Exiting application due to exception")
+        return 1
 
 if __name__ == '__main__':
     # Pyinstaller fix
     multiprocessing.freeze_support()
 
-    try:
-        logger.info("Server is starting")
-        uvicorn.run(ASGIApp(app.socketio, app), host=config.host(), 
-            port=config.port(), reload=config.reload())
-        sys.exit(0)
-    except:
-        logger.exception("exiting application")
-        sys.exit(1)
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        sys.stderr = open('./logs/tsh_error.txt', 'w', encoding='utf-8')
+        sys.stdout = open('./logs/tsh_info.txt', 'w', encoding='utf-8')
+
+    sys.exit(asyncio.run(main()))
--- server/main.py
+++ server/main.py
@@ -1,17 +1,38 @@
-import sys
+import aiofiles
+import aiofiles.os
+import aiofiles.ospath
+import tomllib
 
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from socketio import AsyncServer
-from server import config
 
-if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-    sys.stderr = open('./assets/log_error.txt', 'w', encoding='utf-8')
-    sys.stdout = open('./assets/log.txt', 'w', encoding='utf-8')
+from server import settings
 
-app = FastAPI()
+program_context = {
+    "name": "TournamentStreamHelper",
+    "version": "?",
+    "description": "",
+    "authors": []
+}
+
+async def on_startup(app: FastAPI):
+    async with aiofiles.open('pyproject.toml', mode='r', encoding='utf-8') as f:
+        program_context = tomllib.loads(await f.read())["tool"]["poetry"]
+
+async def on_shutdown(app: FastAPI):
+    await settings.save()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await on_startup(app)
+    yield
+    await on_shutdown(app)
+
+app = FastAPI(lifespan=lifespan)
 app.socketio = AsyncServer(async_mode='asgi')
 templates = Jinja2Templates(directory='./dist')
 
@@ -20,10 +41,7 @@ async def index(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(
         request=request,
         name="index.html",
-        context={
-            "name": config.program_name(),
-            "version": config.program_version()
-        }
+        context=program_context
     )
 
 app.mount("/assets", StaticFiles(directory="./dist/assets"), name="assets")
