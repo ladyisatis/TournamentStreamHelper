da474e967ef05a1babf769236ab4bb0106a213c5
ladyisatis <isatis@foxes.gay>
2024-06-03 18:22:11 -0700

    api_v1_update_team -> update_team_v1

--- server/api/v1/update_team.py
+++ server/api/v1/update_team.py
@@ -1,6 +1,6 @@
 from server.utils.router import method
 from server.api import router_v1
 
-@method("/update_team", router_v1.post, id="api_v1_update_team")
+@method("/update_team", router_v1.post, id="update_team_v1")
 async def update_team(scoreboardNumber: int = 1, team: int = 1, player: int = 1, session_id: str | None = None):
     return scoreboardNumber
